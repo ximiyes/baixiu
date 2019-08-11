7361273e09796269e5d2604db92a5ac01afa0c11
ximi <ximiyes@126.com>
2019-08-12 00:38:42 +0800

    Complete_the_editing_function_of_the_article

--- public/assets/js/post-add.js
+++ public/assets/js/post-add.js
@@ -3,7 +3,7 @@ $.ajax({
     type: "get",
     url: "/categories",
     success: function (response) {
-        console.log(response);
+        // console.log(response);
         let html = template('categoryTpl', {
             list: response
         });
@@ -46,8 +46,8 @@ $('#feature').on('change', function () {
 
 // 给保存按钮添加单击事件  文章添加功能
 $('.btn-primary').on('click', function () {
-    console.log($('#articleForm').serialize());
-    console.log($('#thumbnail').val());
+    // console.log($('#articleForm').serialize());
+    // console.log($('#thumbnail').val());
     // return;
     // $.ajax({
     //     type: "post",
@@ -74,6 +74,106 @@ $('.btn-primary').on('click', function () {
             console.log(err);
         }
 
-    })
+    });
 
-})
+});
+
+
+// 文章编辑功能
+// 添加文章和编辑文章公用一个页面 所以要判断一下是添加还是修改 url地址栏有id参数就是修改 url栏没有id参数就是添加
+
+// 定义一个函数 获取到文章的id  
+// id=5d4fa541dd2b893fc46eab35  这种形式 传入 'id' 可以拿到 id的值
+function getUrlParams(name) {
+    let paramsArr = location.search.substr(1).split('&');
+    // ["id=5d4fa541dd2b893fc46eab35"]
+    // console.log(paramsArr);
+    //循环数组
+    let len = paramsArr.length;
+    for (let i = 0; i < len; i++) {
+        let temp = paramsArr[i].split('=');
+        // console.log(temp);
+        if (temp[0] == name) {
+            return temp[1];
+        }
+    }
+    return -1;
+
+};
+
+// console.log(getUrlParams('id'));
+// 5d4fa541dd2b893fc46eab35
+
+// 获取浏览器地址栏的id参数
+let id = getUrlParams('id');
+// console.log(id);
+// 如果id的值不等于-1 就说明是在进行修改操作
+if (id != -1) {
+    // 根据id 拿到文章的详细信息 
+    $.ajax({
+        type: "get",
+        url: "/posts/" + id,
+        success: function (response) {
+
+            // console.log(response);
+            // 拿到文章详细信息后   还有拿到文章所属分类
+            $.ajax({
+                type: "get",
+                url: "/categories",
+                success: function (categories) {
+                    // 渲染模板  分类信息封装到文章详细信息的对象中  这样我们就能在模板中拿到分类的信息了
+                    // console.log(categories);
+                    // 给response添加一个属性categories 属性值是categories
+                    response.categories = categories;
+                    // console.log(response);
+                    let html = template('modifyArticle', response);
+                    $('.container-fluid').html(html);
+
+
+
+                }
+            });
+        }
+    });
+};
+
+
+
+// 事件格式化函数
+// function formateDate(date) {
+//     // 将日期时间字符串转化为日期对象
+//     // date = new Date(date);
+//     date = new Date(date);
+//     // padStart 不够就补位 用于补0
+//     return date.getFullYear() + '-' + (date.getMonth() + 1).toString().padStart(2, 0) + '-' + date.getDate().toString().padStart(2, 0)+'-'+date.getMinutes().toString().padStart(2, 0)+':'+date.getSeconds().toString().padStart(2, 0);
+// };
+// console.log(formateDate("2019-08-22T03:11:00.000Z"));
+
+
+// 给修改按钮绑定单击事件
+// 由于修改按钮是动态生成的  所以要将单击事件绑定在修改按钮的父元素上
+$('.container-fluid').on('click', '#modifyButton', function () {
+    // 拿到表单提交的数据   
+    // alert('11');
+    // console.log($('#modifyArticleForm'));
+    let formData = $('#modifyArticleForm').serialize();
+    // console.log($('#modifyArticleForm').serialize());
+    // 拿到当前提交数据的id
+    let id = $('#modifyArticleForm').attr('data-id');
+    // console.log(id);
+
+    // 发送ajax请求 拿到当前id 的文章数据  调用接口修改
+    $.ajax({
+        type: "put",
+        url: "/posts/" + id,
+        data: formData,
+        success: function (response) {
+            location.href = '/admin/posts.html';
+        }
+    });
+
+
+
+
+
+});
--- public/assets/js/posts.js
+++ public/assets/js/posts.js
@@ -1,21 +1,39 @@
+// 存储文章数据的数组
+let articleArr = [];
+
 // 渲染所有文章数据
 $.ajax({
     type: "get",
     url: "/posts",
     success: function (response) {
         // console.log(response);
-        let html = template('articleTpl', response);
-        // 将数据写入到tbody中
-        $('tbody').html(html);
+        // let html = template('articleTpl', response);
+        // // 将数据写入到tbody中
+        // $('tbody').html(html);
+        articleArr = response.records;
+        render(articleArr);
+
+        // 渲染分页模板
+        let html = template('pagenationTpl', response);
+        $('.pagination').html(html);
     }
 });
 
+// 渲染文章列表
+function render(arr) {
+    let html = template('articleTpl', {
+        data: arr
+    });
+    $('tbody').html(html);
+}
+
+
 // 事件格式化函数
-function formateDate(date){
+function formateDate(date) {
     // 将日期时间字符串转化为日期对象
     date = new Date(date);
     // padStart 不够就补位 用于补0
-    return date.getFullYear()+'-'+(date.getMonth() + 1).toString().padStart(2,0) + '-' + date.getDate().toString().padStart(2,0);
+    return date.getFullYear() + '-' + (date.getMonth() + 1).toString().padStart(2, 0) + '-' + date.getDate().toString().padStart(2, 0);
 }
 
 // 获取所有分类的数据
@@ -24,23 +42,85 @@ $.ajax({
     url: "/categories",
     success: function (response) {
         // response 拿到的数据是数组 
-        console.log(response);
-        let html = template('categoriesTpl', {list:response});
+        // console.log(response);
+        let html = template('categoriesTpl', {
+            list: response
+        });
         // 将数据插入到分类的下拉栏中
         $('#categorieSelect').html(html);
     }
 });
 
+// 删除文章
+// 根据文章id 删除文章 删除文章以后 重新渲染页面
+// 给删除按钮绑定点击事件  由于删除按钮是动态生成的  所以要把事件绑定在删除按钮的父元素身上
+$('tbody').on('click', '#del', function () {
+    // 拿到点击删除的那条数据的id
+    if (confirm('确定要删除吗')) {
+        let id = $(this).parent().attr('data-id');
+        // alert(id);
+        // 根据这条id 给服务器发送ajax请求
+        $.ajax({
+            type: "delete",
+            url: "/posts/" + id,
+            success: function (response) {
+                // 拿到的是点击删除的那一条数据
+                // console.log(response);
+                // 根据这条数据 去数组里面拿到这条数据的索引 然后将这条数据删除 最后重新渲染页面
+                let index = articleArr.findIndex(item => item._id == id);
+                // alert(index);
+                articleArr.splice(index, 1);
+                render(articleArr);
+            }
+        });
+    }
+});
+
+
+// 文章编辑功能
+// 点击文章编辑  页面跳转到 文章添加页面  并且将点击编辑的那条文章的内容填写到 文章添加页面  修改文章后 调用接口 将这条数据更新到数组里面 最后渲染出来
+// 因为文章编辑也是动态生成的 所以编辑的点击事件绑定在文章编辑的父元素上面
 
 
 
+// 筛选功能
+// 给筛选按钮绑定点击事件 点击筛选按钮 拿到筛选的条件 
+// 向服务器发送ajax请求 拿到数据后 将数据渲染出来
+$('#selectItem').on('click', function () {
+    // 拿到筛选表单里面的数据
+    let selectItemData = $('#filterForm').serialize();
+    console.log(selectItemData);
+    $.ajax({
+        type: "get",
+        url: "/posts",
+        data: selectItemData,
+        success: function (response) {
+            // 返回的是筛选的数据  返回的是对象  与分页的那种对象类似
+            console.log(response);
+            // 取出
+            render(response.records);
+        }
+    });
+});
 
-
-
-
-
-
-
-
-
-
+// 文章分页功能
+// 定义一个函数  形参就是传入的页码 函数拿到传入的页码后 拿到这个页码发送ajax请求 拿到数据 渲染页码模板
+function changePage(page) {
+    $.ajax({
+        type: "get",
+        url: "/posts",
+        data: {
+            page: page
+        },
+        success: function (response) {
+            // console.log(response);
+            let html = template('pagenationTpl', response);
+            // 将数据写入到页码中
+            $('.pagination').html(html);
+            // 拿到当前页的article 将数据渲染到页面中
+            // let htmlArticle = template('articleTpl', {data:response.records});
+            // $('tbody').html(htmlArticle);
+            render(response.records);
+        }
+    });
+};
